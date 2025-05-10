c9e6dbeccb513d7f0bb9bac96fe6af8ffbe8da90
sunny <sunny@bytespark.app>
2025-05-10 17:09:56 +0800

    fix: 修复客户端请求地址

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -1,4 +1,4 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>(process.env.BETTER_AUTH_URL!);
+export const client = hc<AppType>("/");
